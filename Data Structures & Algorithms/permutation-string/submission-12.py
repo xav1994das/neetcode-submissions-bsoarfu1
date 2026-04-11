84b8249218b7a673b30c295e56fe92f8b139636a
xav1994das <xav0anime@gmail.com>
2026-04-11 20:32:41 +0530

    Add: permutation-string - submission-12

---
+++ Data Structures & Algorithms/permutation-string/submission-12.py
@@ -0,0 +1,27 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        count = collections.defaultdict(int)
+        map1 = collections.defaultdict(int)
+        
+        for i in range(len(s1)):
+            count[s1[i]]+=1
+
+        n=len(s2)
+        r=len(s1)
+        l=0
+        if n<r:
+            return n>r
+        for i in range(0,r):
+            map1[s2[i]]+=1 
+        if count==map1:
+            return True
+        while r<n:
+            map1[s2[r]] += 1 
+            r+=1
+            map1[s2[l]] -= 1
+            if map1[s2[l]]==0:
+                map1.pop(s2[l])
+            l+=1
+            if count==map1:
+                return True
+        return False
