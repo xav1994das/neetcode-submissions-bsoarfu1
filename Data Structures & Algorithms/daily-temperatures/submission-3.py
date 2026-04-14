2e638fab38930812ca900744b768878a66dfdcbc
xav1994das <xav0anime@gmail.com>
2026-04-14 12:41:45 +0530

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n=len(temperatures)
+        st=deque()
+        res=[0]*n
+        for i in range(n):
+            j=i
+            while st and temperatures[j]>temperatures[st[-1]]:
+                idx=st[-1]
+                res[idx]=i-idx
+                st.pop()
+            st.append(i)
+        while st:
+            p=st.pop()
+            res[p]=0
+        return res
