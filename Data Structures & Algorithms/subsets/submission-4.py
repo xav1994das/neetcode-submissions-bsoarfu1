b62d35a57f421a82ac988acf6c348108121fedbf
xav1994das <xav0anime@gmail.com>
2026-04-28 14:53:48 +0530

    Add: subsets - submission-4

---
+++ Data Structures & Algorithms/subsets/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res=[]
+        n=len(nums)
+        temp=[]
+        def f(index):
+            nonlocal res,n
+            if index==n:
+                res.append(temp[:])
+                print(temp)
+                return
+            temp.append(nums[index])
+            f(index+1)
+            temp.remove(nums[index])
+            f(index+1)
+
+        
+        f(0)
+        return res
+            
