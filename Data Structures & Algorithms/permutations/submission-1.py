6ca9c0d15a68300581703ab5b67b97e167f01168
xav1994das <xav0anime@gmail.com>
2026-04-29 16:40:12 +0530

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ds,res=[],[]
+        n=len(nums)
+        hmap=collections.defaultdict(int)
+        def backtrack():
+            if len(ds)==n:
+                res.append(ds[:])
+                return
+            for i in range(n):
+                if hmap[nums[i]]==0:
+                    ds.append(nums[i])
+                    hmap[nums[i]]=1
+                    backtrack()
+                    hmap[nums[i]]=0
+                    ds.pop()
+        backtrack()
+        return res
